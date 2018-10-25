7649c5d0449189b97a3c836e4b6925e752312616
JoshK2 <yoshuakuttler@gmail.com>
2018-10-25 19:15:52 +0300

    add example for addition array

--- src/examples/AddNumber.js
+++ src/examples/AddNumber.js
@@ -7,6 +7,7 @@ class AddNumberExamples extends Component {
             <div className="example">
                 <h3>Add Number Examples</h3>
                 <p>1 + 2 = {AddNumber.additionNumbers(1, 2)}</p>
+                <p>[1,2,3] = {AddNumber.additionArray([1,2,3])}</p>
             </div>
         );
     }
