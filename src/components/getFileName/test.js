4c3875e6613eb773a11b583238376ba841020d60
JoshK2 <yoshuakuttler@gmail.com>
2019-01-24 22:14:29 +0200

    add test to getFileName function

--- src/components/getFileName/test.js
+++ src/components/getFileName/test.js
@@ -4,5 +4,9 @@ const currentPath = process.cwd();
 describe('getFileName', function() {
     test('getFileName function return string of file name', () => {
         expect(getFileName(`${currentPath}/src/components/getFileName/index.js`)).toEqual('index.js', "the file name is incorect");
+        expect(getFileName(`/example/components/test/index.js`)).toEqual('index.js', "the file name is incorect");
+        expect(getFileName(`/example/components/test/480dfln.txt`)).toEqual('480dfln.txt', "the file name is incorect");
+        expect(getFileName(`/example/components/test/480dfln.txt/example/components/test/test.json`)).toEqual('test.json', "the file name is incorect");
+
     });
 });
