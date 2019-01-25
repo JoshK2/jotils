e1219a76e2e00f4c98de91a66ada266fad69480d
JoshK2 <yoshuakuttler@gmail.com>
2019-01-25 16:59:42 +0200

    modify main index.js file

--- src/components/index.js
+++ src/components/index.js
@@ -1,23 +1,22 @@
- import random from './random';
- import randomBetween from './randomBetween';
- import randomValueFromArray from './randomValueFromArray';
- import sumArray from './sumArray';
- import sumTwo from './sumTwo';
- import shuffleArray from './shuffleArray';
- import removeDuplicatesInArray from './removeDuplicatesInArray';
- import isBoolean from './isBoolean';
- import isUndefined from './isUndefined';
- import isObject from './isObject';
-
- export { 
-    random,
-    randomBetween,
-    randomValueFromArray,
-    sumArray,
-    sumTwo,
-    shuffleArray,
-    removeDuplicatesInArray,
-    isBoolean,
-    isUndefined,
-    isObject
- };
+export { default as degreesToRadians } from './degreesToRadians';
+export { default as getAllFiles } from './getAllFiles';
+export { default as getFileName } from './getFileName';
+export { default as getSourceFile } from './getSourceFile';
+export { default as isArray } from './isArray';
+export { default as isBoolean } from './isBoolean';
+export { default as isNumber } from './isNumber';
+export { default as isObject } from './isObject';
+export { default as isString } from './isString';
+export { default as isUndefined } from './isUndefined';
+export { default as letterCount } from './letterCount';
+export { default as linearInterpolation } from './linearInterpolation';
+export { default as percentageCalculator } from './percentageCalculator';
+export { default as radiansToDegrees } from './radiansToDegrees';
+export { default as random } from './random';
+export { default as randomBetween } from './randomBetween';
+export { default as randomValueFromArray } from './randomValueFromArray';
+export { default as removeDuplicatesInArray } from './removeDuplicatesInArray';
+export { default as removeValueFromArray } from './removeValueFromArray';
+export { default as shuffleArray } from './shuffleArray';
+export { default as sumArray } from './sumArray';
+export { default as sumTwo } from './sumTwo';
