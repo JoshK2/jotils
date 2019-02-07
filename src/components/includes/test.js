2623a511ec1a599afdb4d36ba8ff68131d446a09
JoshK2 <yoshuakuttler@gmail.com>
2019-02-07 16:24:08 +0200

    add includes array function, and export to bitsrc

---
+++ src/components/includes/test.js
@@ -0,0 +1,13 @@
+import assert from 'assert';
+import includes from './';
+
+describe('includes', function () {
+    it('return true when value is include in array', () => {
+        const result = includes([1, 2, 3], 1);
+        assert(result === true);
+    });
+    it('return false when value is not include in array', () => {
+        const result = includes([1, 2, 3], 4);
+        assert(result === false);
+    });
+});
