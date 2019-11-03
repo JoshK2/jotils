426371cdccbcab3865505913cb48864297c301b0
Josh Kuttler <yoshuakuttler@gmail.com>
2019-11-03 20:25:39 +0200

    update includes function types

--- src/components/includes/includes.ts
+++ src/components/includes/includes.ts
@@ -1,8 +1,8 @@
 /**
  * @description
  * check if value is include in array
- * @param {Array} array array
- * @param {any} value a value to check if is include
+ * @param {Array} array array of string or number
+ * @param {string | number} value a value to check if is include
  * @returns {boolean} true or false if is include or not
  * @example
  * import { includes } from '@bit/joshk.jotils.includes'
@@ -12,6 +12,6 @@
  * import { includes } from '@bit/joshk.jotils.includes'
  * export default includes([1, 2, 3], 4) // => false
  */
-export function includes(array: Array<any>, value: any): boolean {
+export function includes(array: Array<string | number>, value: string | number): boolean {
     return array.includes(value)
 }
