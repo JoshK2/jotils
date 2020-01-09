1e4b8f982935a8f259c9c2c45d2ef0de2380a862
Josh Kuttler <yoshuakuttler@gmail.com>
2020-01-09 18:33:59 +0200

    publish format-number version

--- src/components/format-number/format-number.ts
+++ src/components/format-number/format-number.ts
@@ -1,12 +1,13 @@
 /**
  * @description
  * function that retrun a formated number
+ * @param {string | number} number
  * @returns {string} formated number
  * @example
  * import formatNumber from '@bit/joshk.jotils.format-number'
  * export default formatNumber('123456') // => 123,456
  */
-export function formatNumber(number: string): string {
+export function formatNumber(number: string | number): string {
     number = number.toString()
     return number.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')
 }
